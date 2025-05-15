6f6a35b1f0e151e2a57be306a7ecd4c02300eebb
Jakub Tkaczuk <jtkaczuk@ethz.ch>
2025-05-15 13:31:10 +0200

    this one's the one

--- src/test_new.py
+++ src/test_new.py
@@ -1,18 +1,31 @@
-def test_connection():
-    try:
-        instrument = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
-        instrument.serial.baudrate = 9600
-        instrument.serial.parity = serial.PARITY_NONE
-        
-        # Read module type (40129-40130)
-        module_type = instrument.read_registers(
-            registeraddress=128,  # 40129 - 40001 = 128
-            number_of_registers=2,
-            functioncode=3
-        )
-        hex_value = f"{module_type[0]:04X}{module_type[1]:04X}"
-        return f"Connected to DAM-{hex_value[2:]}"
-    
-    except Exception as e:
-        return f"Connection failed: {str(e)}"
+import minimalmodbus
 
+PORT='/dev/cu.usbserial-AC00XX39'
+TEMP_REGISTER = 100
+HUM_REGISTER = 102
+
+#Set up instrument
+instrument = minimalmodbus.Instrument(PORT,1,mode=minimalmodbus.MODE_RTU)
+
+#Make the settings explicit
+instrument.serial.baudrate = 19200        # Baud
+instrument.serial.bytesize = 8
+instrument.serial.parity   = minimalmodbus.serial.PARITY_EVEN
+instrument.serial.stopbits = 1
+instrument.serial.timeout  = 1          # seconds
+
+# Good practice
+instrument.close_port_after_each_call = True
+
+instrument.clear_buffers_before_each_transaction = True
+
+# Read temperatureas a float
+# if you need to read a 16 bit register use instrument.read_register()
+temperature = instrument.read_float(TEMP_REGISTER)
+
+# Read the humidity
+humidity = instrument.read_float(HUM_REGISTER)
+
+#Pront the values
+print('The temperature is: %.1f deg C\r' % temperature)
+print('The humidity is: %.1f percent\r' % humidity)
